f65b7600959916e45fcbb3253283d64799387767
hanadi3334 <hanadmohamed3334h@gmail.com>
2025-05-13 11:23:15 +0300

    .

---
+++ arrow.js
@@ -0,0 +1,13 @@
+const age = function(){
+
+}
+
+const sum = (x, y) => x + y; 
+
+
+
+// function nam2(y){
+//     console.log(`hello ${y}`)
+// };
+
+const nam2 = y => console.log(`hello ${y}`);
---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(x, y) {
+    return x / y
+    
+};
+
+const square = x => x ** 2;
+
+const add = (x, y) => x + y;
